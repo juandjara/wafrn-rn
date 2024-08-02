a5fdcd60b0d2d385a65206e4fd9a8cb70c5c9b1c
Juan D. Jara <juanorigami@gmail.com>
2024-08-02 14:31:06 +0200

    add skeleton for messages tab and add color to tab bar

--- app/(auth)/(tabs)/_layout.tsx
+++ app/(auth)/(tabs)/_layout.tsx
@@ -1,12 +1,20 @@
-import { MaterialCommunityIcons } from "@expo/vector-icons"
+import { FontAwesome, MaterialCommunityIcons } from "@expo/vector-icons"
 import { Tabs } from "expo-router"
 import colors from "tailwindcss/colors"
 
 export default function TabsLayout() {
   return (
     <Tabs screenOptions={{
-      tabBarInactiveTintColor: colors.gray[200],
-      tabBarActiveTintColor: colors.cyan[500],
+      tabBarInactiveTintColor: colors.indigo[300],
+      tabBarActiveTintColor: colors.gray[300],
+      tabBarInactiveBackgroundColor: colors.blue[950],
+      tabBarActiveBackgroundColor: colors.blue[950],
+      tabBarItemStyle: {
+        backgroundColor: colors.blue[950],
+        borderTopWidth: 1,
+        borderTopColor: colors.blue[900],
+        paddingVertical: 4,
+      },
       tabBarLabelStyle: {
         fontWeight: 'bold',
       }
@@ -15,21 +23,28 @@ export default function TabsLayout() {
         name="index"
         options={{
           title: 'Dashboard',
-          tabBarIcon: ({ color }) => <MaterialCommunityIcons name="home-variant" size={28} color={color} />
-        }}
-      />
-      <Tabs.Screen
-        name="search"
-        options={{
-          title: 'Search',
-          tabBarIcon: ({ color }) => <MaterialCommunityIcons name="magnify" size={28} color={color} />
+          tabBarIcon: ({ color }) => <MaterialCommunityIcons name="home-variant" size={24} color={color} />
         }}
       />
       <Tabs.Screen
         name="notifications"
         options={{
           title: 'Notifications',
-          tabBarIcon: ({ color }) => <MaterialCommunityIcons name="bell-outline" size={28} color={color} />
+          tabBarIcon: ({ color }) => <MaterialCommunityIcons name="bell-outline" size={24} color={color} />
+        }}
+      />
+      <Tabs.Screen
+        name="search"
+        options={{
+          title: 'Search',
+          tabBarIcon: ({ color }) => <MaterialCommunityIcons name="magnify" size={24} color={color} />
+        }}
+      />
+      <Tabs.Screen
+        name="messages"
+        options={{
+          title: 'Messages',
+          tabBarIcon: ({ color }) => <FontAwesome name="envelope-o" size={24} color={color} />
         }}
       />
     </Tabs>
