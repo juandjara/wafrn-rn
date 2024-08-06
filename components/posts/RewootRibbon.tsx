8b6f4fdcf005b5a7c93afaf3f2c537d5b2e4fe38
Juan D. Jara <juanorigami@gmail.com>
2024-08-07 00:31:35 +0200

    add classname prop to RewootRibbon

--- components/posts/RewootRibbon.tsx
+++ components/posts/RewootRibbon.tsx
@@ -4,13 +4,14 @@ import { EvilIcons } from "@expo/vector-icons"
 import { Link } from "expo-router"
 import { Image, Pressable, Text, View } from "react-native"
 import HtmlRenderer from "../HtmlRenderer"
+import clsx from "clsx"
 
-export default function RewootRibbon({ user, userNameHTML }: { user?: PostUser; userNameHTML: string }) {
+export default function RewootRibbon({ user, userNameHTML, className }: { user?: PostUser; userNameHTML: string; className?: string }) {
   const avatar = formatCachedUrl(formatMediaUrl(user?.avatar || ''))
   return (
     <Link href={`/user/${user?.url}`} asChild>
       <Pressable>
-        <View className="pl-1 p-2 flex-row gap-1 items-center bg-blue-950">
+        <View className={clsx(className, 'pl-1 p-2 flex-row gap-1 items-center bg-blue-950')}>
           <EvilIcons name="retweet" size={20} color="white" className="mb-1" />
           <Image
             className="rounded-md border border-gray-500"
