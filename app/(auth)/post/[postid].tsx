17eea58b57c3d3cf5ef8987bb1a8983d0f05f0aa
Juan D. Jara <juanorigami@gmail.com>
2024-08-07 17:32:40 +0200

    extract notes component

--- app/(auth)/post/[postid].tsx
+++ app/(auth)/post/[postid].tsx
@@ -1,30 +1,15 @@
 import Loading from "@/components/Loading"
-import ReplyRibbon from "@/components/posts/ReplyRibbon"
-import RewootRibbon from "@/components/posts/RewootRibbon"
+import Notes from "@/components/posts/Notes"
 import Thread from "@/components/posts/Thread"
 import { getDashboardContext } from "@/lib/api/dashboard"
-import { usePostDescendants, usePostDetail } from "@/lib/api/posts"
+import { usePostDetail } from "@/lib/api/posts"
 import { DashboardContextProvider } from "@/lib/contexts/DashboardContext"
 import { Stack, useLocalSearchParams } from "expo-router"
 import { useMemo } from "react"
-import { RefreshControl, ScrollView, View } from "react-native"
+import { RefreshControl, ScrollView } from "react-native"
 
 export default function PostDetail() {
   const { postid } = useLocalSearchParams()
-  const { data: descendants, isFetching: descendantLoading } = usePostDescendants(postid as string)
-  const notes = useMemo(() => {
-    const users = Object.fromEntries(
-      descendants?.users?.map((u) => [u.id, u]) || []
-    )
-    const posts = descendants?.posts || []
-    const notes = posts.map((post) => ({
-      id: post.type === 'rewoot' ? post.userId : post.id,
-      type: post.type,
-      user: { ...users[post.userId], remoteId: null },
-    }))
-    return notes
-  }, [descendants])
-
   const { data, isFetching, refetch } = usePostDetail(postid as string)
   const { context, post } = useMemo(
     () => {
@@ -58,7 +43,7 @@ export default function PostDetail() {
       <ScrollView
         refreshControl={
           <RefreshControl
-            refreshing={isFetching || descendantLoading}
+            refreshing={isFetching}
             onRefresh={refetch}
           />
         }
@@ -67,23 +52,7 @@ export default function PostDetail() {
           thread={post}
           collapseAncestors={false}
         />
-        {notes.map((note) => {
-          if (note.type === 'rewoot') {
-            return (
-              <View key={note.id} className="border-t border-gray-500">
-                <RewootRibbon user={note.user} userNameHTML={note.user?.name} />
-              </View>
-            )
-          }
-          if (note.type === 'reply') {
-            return (
-              <View key={note.id} className="border-t border-gray-500">
-                <ReplyRibbon user={note.user} postId={note.id} />
-              </View>
-            )
-          }
-          return null
-        }).filter(Boolean)}
+        <Notes postId={postid as string} />
       </ScrollView>
     </DashboardContextProvider>
   )
---
+++ components/posts/Notes.tsx
@@ -0,0 +1,50 @@
+import { usePostDescendants } from "@/lib/api/posts"
+import { useMemo } from "react"
+import { View } from "react-native"
+import RewootRibbon from "./RewootRibbon"
+import ReplyRibbon from "./ReplyRibbon"
+import Loading from "../Loading"
+
+export default function Notes({ postId }: { postId: string }) {
+  const { data: descendants, isFetching } = usePostDescendants(postId)
+  const notes = useMemo(() => {
+    const users = Object.fromEntries(
+      descendants?.users?.map((u) => [u.id, u]) || []
+    )
+    const posts = descendants?.posts || []
+    const notes = posts.map((post) => ({
+      id: post.type === 'rewoot' ? post.userId : post.id,
+      type: post.type,
+      user: { ...users[post.userId], remoteId: null },
+    }))
+    return notes
+  }, [descendants])
+
+  if (isFetching) {
+    return (
+      <Loading />
+    )
+  }
+
+  return (
+    <View id='post-notes'>
+      {notes.map((note) => {
+        if (note.type === 'rewoot') {
+          return (
+            <View key={note.id} className="border-t border-gray-500">
+              <RewootRibbon user={note.user} userNameHTML={note.user?.name} />
+            </View>
+          )
+        }
+        if (note.type === 'reply') {
+          return (
+            <View key={note.id} className="border-t border-gray-500">
+              <ReplyRibbon user={note.user} postId={note.id} />
+            </View>
+          )
+        }
+        return null
+      }).filter(Boolean)}
+    </View>
+  )
+}
