c59df4a20688e1b848732a7fa651a7e8a92673fc
Juan D. Jara <juanorigami@gmail.com>
2024-08-20 02:17:17 +0200

    refactor thread organization of content

--- components/posts/Thread.tsx
+++ components/posts/Thread.tsx
@@ -1,30 +1,33 @@
 import { Post, PostThread } from "@/lib/api/posts.types"
 import { useMemo, useState } from "react"
-import { Text, View } from "react-native"
+import { View } from "react-native"
 import PostFragment from "../dashboard/PostFragment"
 import { useDashboardContext } from "@/lib/contexts/DashboardContext"
 import clsx from "clsx"
-import { isEmptyRewoot } from "@/lib/api/content"
+import { getUserNameHTML, isEmptyRewoot } from "@/lib/api/content"
 import { PrivacyLevel } from "@/lib/api/privacy"
 import { useSettings } from "@/lib/api/settings"
 import { sortPosts } from "@/lib/api/posts"
+import RewootRibbon from "./RewootRibbon"
+import InteractionRibbon from "./InteractionRibbon"
+import { Link } from "expo-router"
 
 const ANCESTOR_LIMIT = 3
 
-export default function Thread({
-  thread,
-  collapseAncestors = true,
-}: {
-  thread: PostThread;
-  collapseAncestors?: boolean
-}) {
+export default function Thread({ thread }: { thread: PostThread }) {
   const { data: settings } = useSettings()
   const [CWOpen, setCWOpen] = useState(false)
   const context = useDashboardContext()
-  const isRewoot = useMemo(() => isEmptyRewoot(thread, context), [thread, context])
+  const { isRewoot, rewootUser, rewootUserName } = useMemo(() => {
+    const isRewoot = isEmptyRewoot(thread, context)
+    const rewootUser = isRewoot ? context.users.find((u) => u.id === thread.userId) : null
+    const rewootUserName = rewootUser ? getUserNameHTML(rewootUser, context) : ''
+    return { isRewoot, rewootUser, rewootUserName }
+  }, [thread, context])
+
   const ancestors = useMemo(() => {
     const sorted = thread.ancestors.sort(sortPosts)
-    if (!collapseAncestors || sorted.length < ANCESTOR_LIMIT) {
+    if (sorted.length < ANCESTOR_LIMIT) {
       return sorted
     }
 
@@ -32,7 +35,15 @@ export default function Thread({
       sorted[0],
       sorted[sorted.length - 1]
     ].filter(Boolean)
-  }, [thread.ancestors, collapseAncestors])
+  }, [thread.ancestors])
+
+  const interactionPost = useMemo(() => {
+    const rewootAncestor = thread.ancestors.find((a) => a.id === thread.parentId)
+    // TODO: consider adding an invariant to check the existance of rewootAncestor
+    return isRewoot && rewootAncestor
+      ? { ...rewootAncestor, notes: thread.notes }
+      : thread
+  }, [isRewoot, thread])
 
   function toggleCW() {
     setCWOpen(o => !o)
@@ -52,28 +63,16 @@ export default function Thread({
     return null
   }
 
-  const hasMore = thread.ancestors.length >= ANCESTOR_LIMIT
-
-  const mainFragment = (
-    <View className={clsx(
-      'bg-indigo-950 border-slate-600',
-      {
-        'border-b': isRewoot,
-        'border-t': !isRewoot && ancestors.length > 0
-      },
-    )}>
-      <PostFragment
-        post={thread}
-        CWOpen={CWOpen}
-        toggleCWOpen={toggleCW}
-      />
-    </View>
-  )
-
   return (
     <View className="mb-4">
-      {isRewoot ? mainFragment : null}
-      {collapseAncestors && hasMore ? (
+      {rewootUser ? (
+        <RewootRibbon
+          user={rewootUser}
+          userNameHTML={rewootUserName}
+          className="border-b border-slate-600"
+        />
+      ) : null}
+      {thread.ancestors.length >= ANCESTOR_LIMIT ? (
         <>
           <View className="bg-indigo-900/50">
             <PostFragment
@@ -83,9 +82,9 @@ export default function Thread({
             />
           </View>
           <View className="mb-[1px] border-b border-t border-cyan-700 bg-blue-900/25">
-            <Text className="text-sm text-white p-2">
+            <Link href={`/post/${interactionPost.id}`} className="text-sm text-white p-2">
               ...{thread.ancestors.length - 2} more posts
-            </Text>
+            </Link>
           </View>
           <View className="bg-indigo-900/50">
             <PostFragment
@@ -112,7 +111,17 @@ export default function Thread({
           </View>
         ))
       )}
-      {isRewoot ? null : mainFragment}
+      <View className={clsx(
+        'bg-indigo-950',
+        { 'border-t border-slate-600': !isRewoot && ancestors.length > 0 },
+      )}>
+        <PostFragment
+          post={thread}
+          CWOpen={CWOpen}
+          toggleCWOpen={toggleCW}
+        />
+        <InteractionRibbon post={interactionPost} />
+      </View>
     </View>
   )
 }
