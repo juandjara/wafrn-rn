5cbdc5f793be2c3257fd6d5fd279f4ce481c9e4f
Juan D. Jara <juanorigami@gmail.com>
2024-10-26 17:06:51 +0200

    update web path detection

--- lib/webPathToAppPath.ts
+++ lib/webPathToAppPath.ts
@@ -1,16 +1,16 @@
 
 // transform a web url to a native url
 export default function webPathToAppPath(webPath: string) {
-  if (webPath.startsWith('/blog/')) {
-    const user = webPath.replace('/blog/', '')
+  if (webPath.startsWith('blog/')) {
+    const user = webPath.replace('blog/', '')
     return `/user/${user}`
   }
-  if (webPath.startsWith('/post/') || webPath.startsWith('/fediverse/post/')) {
-    const postId = webPath.replace('/fediverse/post/', '').replace('/post/', '')
+  if (webPath.startsWith('post/') || webPath.startsWith('fediverse/post/')) {
+    const postId = webPath.replace('fediverse/post/', '').replace('post/', '')
     return `/post/${postId}`
   }
-  if (webPath.startsWith('/dashboard/search/')) {
-    const q = webPath.replace('/dashboard/search/', '')
+  if (webPath.startsWith('dashboard/search/')) {
+    const q = webPath.replace('dashboard/search/', '')
     return `/search/${q}`
   }
   return '/'
