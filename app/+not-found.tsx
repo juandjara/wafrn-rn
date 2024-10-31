8fe1b3fcfa8079f2006b0a2cb9d212f38fe0cade
Juan D. Jara <juanorigami@gmail.com>
2024-10-31 18:06:43 +0100

    try one more thing for not-found screen

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -1,25 +1,39 @@
-import { Link, router, Stack, useFocusEffect, useUnstableGlobalHref } from 'expo-router'
-import { StyleSheet } from 'react-native'
+import { Link, router, Stack, useFocusEffect, usePathname } from 'expo-router'
+import { StyleSheet, View } from 'react-native'
 import { ThemedText } from '@/components/ThemedText'
 import { ThemedView } from '@/components/ThemedView'
-import { useWebLinkDetector } from '@/lib/weblinks'
+import webUrlToAppUrl, { useWebLinkDetector } from '@/lib/weblinks'
+import { useCallback } from 'react'
+import { BASE_URL } from '@/lib/config'
 
 export default function NotFoundScreen() {
-  const href = useUnstableGlobalHref()
-  console.log(`not found: ${href}`)
+  const pathname = usePathname()
 
-  const weblink = useWebLinkDetector()
-  useFocusEffect(() => {
-    if (weblink) {
-      router.replace(weblink)
+  const initialLink = useWebLinkDetector()
+
+  const focusEffectFn = useCallback(() => {
+    if (initialLink) {
+      router.replace(initialLink)
     }
-  })
+    if (pathname.startsWith('/fediverse/') || pathname.startsWith('/dashboard/search')) {
+      const link = webUrlToAppUrl(`${BASE_URL}${pathname}`)
+      if (link) {
+        router.replace(link)
+      }
+    }
+  }, [initialLink, pathname])
+
+  useFocusEffect(focusEffectFn)
 
   return (
     <>
       <Stack.Screen options={{ title: 'Oops!' }} />
       <ThemedView style={styles.container}>
-        <ThemedText type="defaultSemiBold" className='mb-2'>{href}</ThemedText>
+        <View className="mb-4">
+          <ThemedText type="defaultSemiBold" className="text-sm">
+            not found: {pathname}
+          </ThemedText>
+        </View>
         <ThemedText type="title">
           This screen doesn't exist
         </ThemedText>
