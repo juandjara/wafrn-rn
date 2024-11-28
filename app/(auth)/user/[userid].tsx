f4c0d080f44c5f51f4d4c90af2da8b9793c5faf3
Juan D. Jara <juanorigami@gmail.com>
2024-11-28 23:55:16 +0100

    disable buggy screen

--- app/(auth)/user/[userid].tsx
+++ app/(auth)/user/[userid].tsx
@@ -136,8 +136,6 @@ export default function UserFeed() {
         title: headerTitle,
         headerBackTitle: 'Back',
         headerTransparent: true,
-        presentation: 'fullScreenModal',
-        animation: 'fade',
         headerTitle: ({ children, tintColor }) => (
           <Reanimated.Text
             numberOfLines={1}
