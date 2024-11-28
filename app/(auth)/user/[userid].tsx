4ad6821277405c8e05a329a66d21f8483f759740
Juan D. Jara <juanorigami@gmail.com>
2024-11-28 12:26:10 +0100

    transform user page list data like post detail list data

--- app/(auth)/user/[userid].tsx
+++ app/(auth)/user/[userid].tsx
@@ -6,18 +6,23 @@ import UserDetail from "@/components/user/UserDetail"
 import { Colors } from "@/constants/Colors"
 import { dedupePosts, getDashboardContext, useUserFeed } from "@/lib/api/dashboard"
 import { PostThread } from "@/lib/api/posts.types"
-import { useUser } from "@/lib/api/user"
+import { User, useUser } from "@/lib/api/user"
 import { DashboardContextProvider } from "@/lib/contexts/DashboardContext"
 import { buttonCN } from "@/lib/styles"
 import useSafeAreaPadding from "@/lib/useSafeAreaPadding"
 import { FlashList, FlashListProps } from "@shopify/flash-list"
 import { useQueryClient } from "@tanstack/react-query"
 import { router, Stack, useLocalSearchParams } from "expo-router"
-import { useMemo } from "react"
+import { useCallback, useMemo } from "react"
 import { Pressable, Text, View } from "react-native"
 import Reanimated, { interpolate, interpolateColor, useAnimatedScrollHandler, useAnimatedStyle, useSharedValue } from "react-native-reanimated"
 
-const AnimatedFlashList = Reanimated.createAnimatedComponent<FlashListProps<PostThread>>(FlashList)
+type UserListItem =
+  | { type: 'user', user: User }
+  | { type: 'error', error: Error }
+  | { type: 'post', post: PostThread }
+
+const AnimatedFlashList = Reanimated.createAnimatedComponent<FlashListProps<UserListItem>>(FlashList)
 
 export default function UserFeed() {
   const { userid } = useLocalSearchParams()
@@ -41,11 +46,40 @@ export default function UserFeed() {
     })
   }
 
-  const context = useMemo(
-    () => getDashboardContext(feed?.pages || []),
-    [feed?.pages]
-  )
-  const deduped = useMemo(() => dedupePosts(feed?.pages || []), [feed?.pages])
+  const { context, listData } = useMemo(() => {
+    const context = getDashboardContext(feed?.pages || [])
+    const posts = dedupePosts(feed?.pages || [])
+    
+    const listData = [
+      user && { type: 'user' as const, user },
+      feedError && { type: 'error' as const, error: feedError },
+      ...posts.map((post) => ({ type: 'post' as const, post })),
+    ].filter(d => !!d)
+
+    return { context, listData }
+  }, [feedError, feed?.pages, user])
+
+  const renderListItem = useCallback(({ item }: { item: UserListItem }) => {
+    if (item.type === 'post') {
+      return <Thread thread={item.post} />
+    }
+    if (item.type === 'user' && item.user) {
+      return <UserDetail user={item.user} />
+    }
+    if (item.type === 'error' && item.error) {
+      return (
+        <View className="m-1 p-2 bg-red-500/30 rounded-md">
+          <Text className="text-white mb-2 font-bold">
+            Error fetching user posts:
+          </Text>
+          <Text className="text-gray-200">
+            {item.error.message}
+          </Text>
+        </View>
+      )
+    }
+    return null
+  }, [])
 
   const sx = useSafeAreaPadding()
   const scrollY = useSharedValue(0)
@@ -70,6 +104,8 @@ export default function UserFeed() {
     )
   }))
 
+  const headerTitle = String(userid).startsWith('@') ? String(userid) : `@${userid}`
+
   if (userError) {
     return (
       <ThemedView className="p-3 flex-1 justify-center items-center">
@@ -92,8 +128,6 @@ export default function UserFeed() {
       </ThemedView>
     )
   }
-
-  const headerTitle = String(userid).startsWith('@') ? String(userid) : `@${userid}`
 
   return (
     <DashboardContextProvider data={context}>
@@ -118,26 +152,12 @@ export default function UserFeed() {
         scrollEventThrottle={16}
         estimatedItemSize={800}
         contentContainerStyle={{ paddingTop: sx.paddingTop }}
-        data={deduped}
+        data={listData}
         refreshing={feedFetching || userFetching}
         onRefresh={refresh}
-        ListHeaderComponent={
-          <>
-            {user && <UserDetail user={user} />}
-            {feedError ? (
-              <View className="m-1 p-2 bg-red-500/30 rounded-md">
-                <Text className="text-white mb-2 font-bold">
-                  Error fetching user posts:
-                </Text>
-                <Text className="text-gray-200">
-                  {feedError.message}
-                </Text>
-              </View>
-            ) : null}
-          </>
-        }
-        keyExtractor={(item) => item.id}
-        renderItem={({ item }) => <Thread thread={item} />}
+        getItemType={(item) => item.type}
+        keyExtractor={(item) => item.type === 'post' ? item.post.id : item.type}
+        renderItem={renderListItem}
         onEndReached={() => hasNextPage && !feedFetching && fetchNextPage()}
         ListEmptyComponent={
           <View className="py-4">
