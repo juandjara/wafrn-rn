a9dcf153fc228cd977cbc865957b8e8f9231cc8c
Juan D. Jara <juanorigami@gmail.com>
2024-12-03 01:20:52 +0100

    save downloaded file with expo-media-library

--- lib/downloads.ts
+++ lib/downloads.ts
@@ -2,6 +2,7 @@ import { cacheDirectory, makeDirectoryAsync, getInfoAsync, downloadAsync } from 
 import { unfurlCacheUrl } from './formatters'
 import { showToast } from './interaction'
 import colors from 'tailwindcss/colors'
+import { saveToLibraryAsync } from 'expo-media-library'
 
 const CACHE_DIR = `${cacheDirectory}WAFRN/`
 
@@ -13,12 +14,18 @@ async function ensureDownloadDirectory() {
   }
 }
 
+async function saveFileToGallery(localUrl: string) {
+  await saveToLibraryAsync(localUrl)
+}
+
 export async function downloadFile(url: string) {
   const name = unfurlCacheUrl(url).split('/').pop()
   try {
     await ensureDownloadDirectory()
     const file = await downloadAsync(url, `${CACHE_DIR}${name}`)
     console.log('Downloaded file', file.uri)
+    await saveFileToGallery(file.uri)
+    console.log('Saved file to gallery')
     showToast('Downloaded file', colors.green[100], colors.green[900])
   } catch (e) {
     console.error('Failed to download file', e)
