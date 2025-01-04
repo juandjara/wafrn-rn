cbd9ae7596657b86f21e4f79a9749beed779554e
Juan D. Jara <juanorigami@gmail.com>
2025-01-04 01:03:44 +0100

    remove static config

--- lib/config.ts
+++
@@ -1,5 +0,0 @@
-export const BASE_URL = "https://app.wafrn.net"
-export const API_URL = `${BASE_URL}/api`
-export const CACHE_HOST = 'cdn.wafrn.net'
-export const CACHE_URL = `https://${CACHE_HOST}/api/cache?media=`
-export const MEDIA_URL = `${BASE_URL}/api/uploads`
