9a6c510af0ca8e22dbf5d936a4fa86e3b6287b5a
Juan D. Jara <juanorigami@gmail.com>
2025-01-17 14:03:29 +0100

    alt text toggle

--- components/posts/Media.tsx
+++ components/posts/Media.tsx
@@ -1,17 +1,19 @@
 import { PostMedia } from "@/lib/api/posts.types";
 import { formatCachedUrl, formatMediaUrl } from "@/lib/formatters";
-import { Text, View } from "react-native";
+import { Pressable, ScrollView, Text, View } from "react-native";
 import { getAspectRatio, isAudio, isImage, isVideo } from "@/lib/api/media";
 import ZoomableImage from "./ZoomableImage";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { Link } from "expo-router";
 import MediaCloak from "./MediaCloak";
 import Video from "../Video";
+import { useState } from "react";
 
 export default function Media({ media, contentWidth }: {
   media: PostMedia
   contentWidth: number
 }) {
+  const [showAlt, setShowAlt] = useState(false)
   const mime = media.mediaType
   const src = formatCachedUrl(formatMediaUrl(media.url))
   const aspectRatio = getAspectRatio(media)
@@ -70,7 +72,7 @@ export default function Media({ media, contentWidth }: {
   }
 
   return (
-    <View className="overflow-hidden mb-2 border border-gray-300 rounded-lg">
+    <View className="overflow-hidden mb-2 border border-gray-300 rounded-lg relative">
       <MediaCloak
         blurHash={media.blurhash}
         isNSFW={media.NSFW}
@@ -82,9 +84,27 @@ export default function Media({ media, contentWidth }: {
       >
         {content}
       </MediaCloak>
-      <Text className="text-white text-xs p-2 bg-blue-950 rounded-lg">
+      {/* <Text className="text-white text-xs p-2 bg-blue-950 rounded-lg">
         {media.description || 'no alt text'}
-      </Text>
+      </Text> */}
+      <Pressable
+        onPress={() => setShowAlt(prev => !prev)}
+        className="absolute top-0 left-0 rounded-br-md bg-indigo-950/75 p-2"
+      >
+        <Text className="text-white text-xs font-semibold">ALT</Text>
+      </Pressable>
+      {showAlt && (
+        <View
+          style={{ maxHeight: '50%', paddingBottom: 4, backgroundColor: 'rgba(0,0,0,0.75)' }}
+          className="absolute z-10 bottom-0 left-0 right-0 py-2 px-3"
+        >
+          <ScrollView>
+            <Text className="text-white text-center">
+              {media.description || 'no alt text'}
+            </Text>
+          </ScrollView>
+        </View>
+      )}
     </View>
   )
 }
