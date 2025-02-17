6dee72a4b1d0f9382d27205c7efbb6367e8fc877
Juan D. Jara <juanorigami@gmail.com>
2025-02-17 11:28:58 +0100

    disable react query retry

--- lib/queryClient.ts
+++ lib/queryClient.ts
@@ -5,6 +5,8 @@ export const queryClient = new QueryClient({
     queries: {
       refetchOnWindowFocus: false,
       throwOnError: true,
+      // don't retry on error, we want to fail fast, retry controls should be explicit and handled by the caller
+      retry: false,
     }
   }
 })
