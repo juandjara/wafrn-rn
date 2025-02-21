e63b0d797e6193bda0af951faf91b6c8aab0b3c1
Juan D. Jara <juanorigami@gmail.com>
2025-02-22 00:23:42 +0100

    configure token registration

--- lib/push-notifications.ts
+++ lib/push-notifications.ts
@@ -3,8 +3,13 @@ import * as Device from 'expo-device';
 import * as Notifications from 'expo-notifications';
 import Constants from 'expo-constants';
 import { useEffect } from "react";
-import { useState } from "react";
 import { showToastError } from "./interaction";
+import { useAuth } from "./contexts/AuthContext";
+import { getEnvironmentStatic } from "./api/auth";
+import { getJSON } from "./http";
+import { Notification, useNotificationBadges } from "./notifications";
+import { router } from "expo-router";
+import useAsyncStorage from "./useLocalStorage";
 
 Notifications.setNotificationHandler({
   handleNotification: async () => ({
@@ -14,7 +19,19 @@ Notifications.setNotificationHandler({
   }),
 });
 
-async function setupPushNotifications() {
+async function registerPushNotificationToken(authToken: string, expoToken: string) {
+  const env = getEnvironmentStatic()
+  await getJSON(`${env?.API_URL}/v3/registerNotificationToken`, {
+    method: 'POST',
+    body: JSON.stringify({ token: expoToken }),
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${authToken}`
+    }
+  })
+}
+
+async function setupPushNotifications(authToken: string) {
   if (Platform.OS === 'android') {
     Notifications.setNotificationChannelAsync('default', {
       name: 'default',
@@ -44,43 +61,73 @@ async function setupPushNotifications() {
     }
 
     const token = await Notifications.getExpoPushTokenAsync({ projectId })
+    await registerPushNotificationToken(authToken, token.data)
+
     return token.data
   } else {
     throw new Error('Must use physical device to setup push notifications')
   }
 }
 
+type PushNotificationPayload = {
+  notification: Notification
+  context?: {
+    postContent?: string
+    userUrl?: string
+    emoji?: string
+  }
+}
 
 export function usePushNotifications() {
-  const [token, setToken] = useState<string | null>(null)
+  const { token: authToken } = useAuth()
+  const { refetch: refetchBadges } = useNotificationBadges()
+  const { value: expoToken, loading, setValue: setExpoToken } = useAsyncStorage<string>('pushNotificationToken')
 
   useEffect(() => {
-    setupPushNotifications()
-      .then((token) => setToken(token))
-      .catch((err) => {
-        console.error(err)
-        if (err instanceof Error) {
-          showToastError(err.message)
-        } else {
-          showToastError('Failed to setup push notifications')
-        }
-      })
+    if (!authToken || loading) {
+      return
+    }
+
+    if (!expoToken) {
+      setupPushNotifications(authToken)
+        .then((token) => setExpoToken(token))
+        .catch((err) => {
+          console.error(err)
+          if (err instanceof Error) {
+            showToastError(err.message)
+          } else {
+            showToastError('Failed to setup push notifications')
+          }
+        })
+    }
+
+    // parse app link from notification
+    function processNotificationClick(data: PushNotificationPayload) {
+      if (data.notification.notificationType === 'FOLLOW') {
+        const url = `/user/${data.context?.userUrl}`
+        router.navigate(url)
+      } else {
+        const postId = data.notification.postId
+        const url = `/post/${postId}`
+        router.navigate(url)
+      }
+    }
 
     const subscription = Notifications.addNotificationReceivedListener((notification) => {
       console.log('Notification received while the app is running', notification)
-      // TODO:update the notification count
+      refetchBadges()
     })
 
     const subscription2 = Notifications.addNotificationResponseReceivedListener((response) => {
       console.log('Notification pressed while the app is running', response)
       // TODO: navigate to the notification link
+      const notif = response.notification.request.content.data as PushNotificationPayload
+      processNotificationClick(notif)
     })
 
     return () => {
       Notifications.removeNotificationSubscription(subscription)
       Notifications.removeNotificationSubscription(subscription2)
     }
-  }, [])
-
-  return token
+  }, [authToken, refetchBadges, expoToken, loading, setExpoToken])
 }
