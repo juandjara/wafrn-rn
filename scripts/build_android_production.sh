a70ebe9a70b9216204714e5c98e0537b99ed3a34
Juan D. Jara <juanorigami@gmail.com>
2025-05-05 18:15:05 +0200

    [skpi ci] move android build script to scripts folder

--- scripts/build_android_production.sh
+++ scripts/build_android_production.sh
@@ -1,12 +1,21 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 set -euxo pipefail
+
+cd "$(dirname "$0")"
+cd ..
 
 echo 'Hello!'
 echo 'This script will create an optimized production build for android, creating different apk builds for different platforms and an universal apk that merges all of them'
 echo 'first we will remove expo-notifications to remove android dependencies of google and firebase. We cannot directly remove it everywhere since the iOS app still uses it'
 
 npm un expo-notifications
+
+if ! [ -f 'android/gradlew' ]; then
+  echo 'android/gradlew not found'
+  exit 1
+fi
+
 ./android/gradlew app:assembleRelease
 
 echo 'now we will install expo-notifications again to not break the iOS build'
