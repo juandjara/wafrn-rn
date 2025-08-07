f142afad7c669983240dc50c977ff780625965b1
Juan D. Jara <juanorigami@gmail.com>
2025-08-07 11:35:18 +0200

    type fix for ios notifs

--- lib/push-notifications/push-notifications.ios.ts
+++ lib/push-notifications/push-notifications.ios.ts
@@ -14,6 +14,8 @@ Notifications.setNotificationHandler({
     shouldShowAlert: true,
     shouldPlaySound: false,
     shouldSetBadge: false,
+    shouldShowBanner: true,
+    shouldShowList: true,
   }),
 })
 
