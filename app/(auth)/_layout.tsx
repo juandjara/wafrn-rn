06d9973bafbc86c1d4662a8f25dc9b573f3d33b4
Juan D. Jara <juanorigami@gmail.com>
2025-08-29 03:07:01 +0200

    remove console.log

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -4,8 +4,7 @@ import { getRootStyles } from '@/constants/Colors'
 import checkExpoUpdates from '@/lib/checkExpoUpdates'
 import { useAuth } from '@/lib/contexts/AuthContext'
 import useAppFocusListener from '@/lib/useAppFocusListener'
-import { Redirect, Stack, useNavigation } from 'expo-router'
-import { useEffect } from 'react'
+import { Redirect, Stack } from 'expo-router'
 import { useColorScheme } from 'react-native'
 
 export const unstable_settings = {
@@ -15,22 +14,8 @@ export const unstable_settings = {
 export default function ProtectedLayout() {
   const { token, env, isLoading } = useAuth()
   const rootStyles = getRootStyles(useColorScheme() ?? 'dark')
-  const navigation = useNavigation('/')
 
   useAppFocusListener(checkExpoUpdates)
-
-  useEffect(() => {
-    const unsub1 = navigation.addListener('blur', (ev) => {
-      console.log('ROUTER BLUR', ev)
-    })
-    const unsub2 = navigation.addListener('focus', (ev) => {
-      console.log('ROUTER FOCUS', ev)
-    })
-    return () => {
-      unsub1()
-      unsub2()
-    }
-  }, [navigation])
 
   if (isLoading) {
     return <SplashScreen />
