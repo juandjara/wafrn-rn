9d978ab8c7d107ec3708475a5b56eb0060b3008f
Juan D. Jara <juanorigami@gmail.com>
2025-11-12 23:02:16 +0100

    new eslint flat config

---
+++ eslint.config.js
@@ -0,0 +1,12 @@
+// https://docs.expo.dev/guides/using-eslint/
+const { defineConfig } = require('eslint/config')
+const expoConfig = require('eslint-config-expo/flat')
+const prettierConfig = require('eslint-plugin-prettier/recommended')
+
+module.exports = defineConfig([
+  expoConfig,
+  prettierConfig,
+  {
+    ignores: ['dist/*', '.expo', 'node_modules'],
+  },
+])
