5af9be4f5395a7e4c5e9550a5842eddd0bea1771
Juan D. Jara <juanorigami@gmail.com>
2025-11-19 02:22:34 +0100

    apply suggested change in kotlin from react-native-screens to fix crashes

--- android/app/src/main/java/dev/djara/wafrn_rn/MainActivity.kt
+++ android/app/src/main/java/dev/djara/wafrn_rn/MainActivity.kt
@@ -8,6 +8,7 @@ import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
+import com.swmansion.rnscreens.fragment.restoration.RNScreensFragmentFactory
 
 import expo.modules.ReactActivityDelegateWrapper
 
@@ -20,7 +21,9 @@ class MainActivity : ReactActivity() {
     // @generated begin expo-splashscreen - expo prebuild (DO NOT MODIFY) sync-f3ff59a738c56c9a6119210cb55f0b613eb8b6af
     SplashScreenManager.registerOnActivity(this)
     // @generated end expo-splashscreen
-    super.onCreate(null)
+    // as suggested by https://github.com/software-mansion/react-native-screens?tab=readme-ov-file#android
+    supportFragmentManager.fragmentFactory = RNScreensFragmentFactory()
+    super.onCreate(savedInstanceState)
   }
 
   /**
