104fb7529992e1214661570f867f9d330ff34152
Juan D. Jara <juanorigami@gmail.com>
2025-11-23 01:14:43 +0100

    check for check

--- scripts/checkEnvironment.ts
+++ scripts/checkEnvironment.ts
@@ -1,17 +1,26 @@
 #!/usr/bin/env node
 
-const fs = require('fs');
-const path = require('path');
-const ROOT = path.resolve(__dirname, '..');
+const fs = require('fs')
+const path = require('path')
 
-const GRADLE_FILE = path.resolve(ROOT, 'android/app/build.gradle');
+let baseDir = ''
+if (typeof __dirname !== 'undefined') {
+  baseDir = __dirname
+} else {
+  baseDir = process.cwd()
+}
+const ROOT = path.resolve(baseDir, '..')
+
+const GRADLE_FILE = path.resolve(ROOT, 'android/app/build.gradle')
 
 function main() {
-  const gradleFile = fs.readFileSync(GRADLE_FILE, 'utf8');
+  const gradleFile = fs.readFileSync(GRADLE_FILE, 'utf8')
   const test = "applicationId 'dev.djara.wafrn_rn.dev'"
 
   if (gradleFile.includes(test)) {
-    throw new Error('Development environment detected. Deploying to production is not allowed. Run `npm run setup:prod` to set up the production environment.');
+    throw new Error(
+      'Development environment detected. Deploying to production is not allowed. Run `npm run setup:prod` to set up the production environment.',
+    )
   }
 }
 main()
