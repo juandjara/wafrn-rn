2a6666f94c11d940a1aec888499e50e3d7312ba5
Juan D. Jara <juanorigami@gmail.com>
2025-12-14 21:50:03 +0100

    bring back memo on toasts

--- lib/toasts.ts
+++ lib/toasts.ts
@@ -15,68 +15,70 @@ export function useToasts() {
   const red500 = useCSSVariable('--color-red-500') as string
   const black = useCSSVariable('--color-black') as string
 
-  function showToastSuccess(message: string) {
-    toast.success(message, {
-      styles: {
-        text: {
-          color: green900,
+  return useMemo(() => {
+    function showToastSuccess(message: string) {
+      toast.success(message, {
+        styles: {
+          text: {
+            color: green900,
+          },
+          view: {
+            backgroundColor: green100,
+            borderRadius: 8,
+          },
         },
-        view: {
-          backgroundColor: green100,
-          borderRadius: 8,
-        },
-      },
-    })
-  }
+      })
+    }
 
-  function showToastError(message: string) {
-    toast.error(message, {
-      styles: {
-        text: {
-          color: red900,
+    function showToastError(message: string) {
+      toast.error(message, {
+        styles: {
+          text: {
+            color: red900,
+          },
+          view: {
+            backgroundColor: red100,
+            borderRadius: 8,
+          },
         },
-        view: {
-          backgroundColor: red100,
-          borderRadius: 8,
-        },
-      },
-    })
-  }
+      })
+    }
 
-  function showToastDarkSouls(message: string) {
-    toast(message.toUpperCase(), {
-      duration: 3000,
-      styles: {
-        text: {
-          color: red500,
-          fontSize: 24,
+    function showToastDarkSouls(message: string) {
+      toast(message.toUpperCase(), {
+        duration: 3000,
+        styles: {
+          text: {
+            color: red500,
+            fontSize: 24,
+          },
+          view: {
+            backgroundColor: black,
+            borderRadius: 8,
+          },
         },
-        view: {
-          backgroundColor: black,
-          borderRadius: 8,
-        },
-      },
-    })
-  }
+      })
+    }
 
-  function showToastInfo(message: string) {
-    toast(message, {
-      styles: {
-        text: {
-          color: blue900,
+    function showToastInfo(message: string) {
+      toast(message, {
+        styles: {
+          text: {
+            color: blue900,
+          },
+          view: {
+            backgroundColor: white,
+            borderRadius: 8,
+          },
         },
-        view: {
-          backgroundColor: white,
-          borderRadius: 8,
-        },
-      },
-    })
-  }
+      })
+    }
 
-  return {
-    showToastSuccess,
-    showToastError,
-    showToastDarkSouls,
-    showToastInfo,
-  }
+    return {
+      showToastSuccess,
+      showToastError,
+      showToastDarkSouls,
+      showToastInfo,
+    }
+  }, [green100, green900, red100, red500, red900, blue900, black, white])
 }
