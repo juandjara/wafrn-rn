30c54214bc1ec0fac77f8a70f8b1e0b4e5341011
Juan D. Jara <juanorigami@gmail.com>
2025-12-18 12:20:23 +0100

    THIS WAS IT

--- lib/push-notifications/push-notifications.android.ts
+++ lib/push-notifications/push-notifications.android.ts
@@ -1,12 +1,13 @@
-import { useEffect } from 'react'
+import { useEffect, useLayoutEffect, useRef } from 'react'
 import { useAuth } from '../contexts/AuthContext'
 import {
+  notificationCleanup,
   registerUnifiedPush,
   unregisterUnifiedPush,
   useNotificationBadges,
-  useNotificationCleanupMutation,
 } from '../notifications'
 import ExpoUnifiedPush, {
+  CallbackData,
   RegisteredPayload,
   checkPermissions,
   requestPermissions,
@@ -15,7 +16,6 @@ import ExpoUnifiedPush, {
 import useAsyncStorage from '../useLocalStorage'
 import { AppState } from 'react-native'
 import useAppFocusListener from '../useAppFocusListener'
-import { useMutation } from '@tanstack/react-query'
 import { parseToken } from '../api/auth'
 
 export function getSavedDistributor() {
@@ -44,110 +44,117 @@ async function checkNotificationPermissions() {
   }
 }
 
+async function notificationSetup(token: string, serverVAPIDKey?: string) {
+  if (!serverVAPIDKey) {
+    throw new Error('webpushPublicKey not found in instance environment')
+  }
+
+  const tokenData = parseToken(token)
+  if (!tokenData) {
+    throw new Error('Invalid token')
+  }
+
+  const distributors = getDistributors()
+  const savedDistributor = getSavedDistributor()
+  if (!savedDistributor) {
+    // NOTE: initial implementation will always use the first distributor if no saved distributor is found,
+    // but we allow the user to select the distributor they want to use in the settings
+    saveDistributor(distributors[0].id)
+  }
+
+  const granted = await checkNotificationPermissions()
+  if (granted) {
+    await registerDevice(serverVAPIDKey, tokenData.userId)
+  } else {
+    throw new Error('Notification permissions not granted')
+  }
+
+  // make sure there is no residual data from previous expo notifications
+  await notificationCleanup(token, { deleteExpo: true, deleteUP: false })
+}
+
 export function usePushNotifications() {
-  const { token, env } = useAuth()
+  const { token, env, envStatus } = useAuth()
   const tokenData = parseToken(token)
 
   const { value: upData, setValue: setUpData } =
     useAsyncStorage<RegisteredPayload>(`UnifiedPushData-${tokenData?.userId}`)
   const { refetch: refetchBadges } = useNotificationBadges()
-  const SERVER_VAPID_KEY = env?.SERVER_VAPID_KEY
-  const notificationCleanup = useNotificationCleanupMutation()
 
-  useAppFocusListener(refetchBadges)
+  useAppFocusListener(refetchBadges, false)
 
-  const setupMutation = useMutation({
-    mutationKey: ['notificationSetup', token],
-    mutationFn: async () => {
-      if (!SERVER_VAPID_KEY) {
-        throw new Error('webpushPublicKey not found in instance environment')
+  useEffect(() => {
+    if (!!token && envStatus === 'success') {
+      // in dev mode, we don't want to register or store data for push notifications to avoid duplicated notifications
+      if (__DEV__) {
+        console.log('Not running notification setup mutation on __DEV__')
+        return
       }
 
-      const tokenData = parseToken(token)
-      if (!tokenData) {
-        throw new Error('Invalid token')
-      }
+      notificationSetup(token, env?.SERVER_VAPID_KEY).catch((err) => {
+        console.error('Error setting up notifications:', err)
+      })
+    }
+  }, [token, envStatus, env?.SERVER_VAPID_KEY])
 
-      const distributors = getDistributors()
-      const savedDistributor = getSavedDistributor()
-      if (!savedDistributor) {
-        // NOTE: initial implementation will always use the first distributor if no saved distributor is found,
-        // but we allow the user to select the distributor they want to use in the settings
-        saveDistributor(distributors[0].id)
+  async function notificationCallback(message: CallbackData) {
+    if (message.action === 'registered' && token) {
+      console.log(
+        `[expo-unified-push] registered user ${message.data.instance} with url ${message.data.url}`,
+      )
+      try {
+        await registerUnifiedPush(token, message.data)
+        await setUpData(message.data)
+      } catch (error) {
+        console.error(
+          '[expo-unified-push] error in registerUnifiedPush: ',
+          error,
+        )
       }
+    }
 
-      const granted = await checkNotificationPermissions()
-      if (granted) {
-        await registerDevice(SERVER_VAPID_KEY, tokenData.userId)
-      } else {
-        throw new Error('Notification permissions not granted')
+    if (message.action === 'unregistered' && token && upData) {
+      console.log(
+        `[expo-unified-push] unregistered user ${message.data.instance} with url ${upData.url}`,
+      )
+      try {
+        await unregisterUnifiedPush(token, upData)
+        await setUpData(null)
+      } catch (error) {
+        console.error(
+          '[expo-unified-push] error in unregisterUnifiedPush: ',
+          error,
+        )
       }
-    },
-    onSettled: () => {
-      // make sure there is no residual data from previous expo notifications
-      notificationCleanup.mutate({ deleteExpo: true, deleteUP: false })
-    },
-    onError: (error) => {
-      console.error('Error setting up notifications:', error)
-    },
+    }
+
+    if (message.action === 'error') {
+      console.error('[expo-unified-push] error: ', message.data)
+    }
+
+    if (message.action === 'registrationFailed') {
+      console.error('[expo-unified-push] registrationFailed: ', message.data)
+    }
+
+    if (message.action === 'message' && AppState.currentState === 'active') {
+      console.log('[expo-unified-push] message: ', message)
+      await refetchBadges()
+    }
+  }
+  const notificationCallbackRef = useRef(notificationCallback)
+
+  useLayoutEffect(() => {
+    notificationCallbackRef.current = notificationCallback
   })
 
   useEffect(() => {
-    // in dev mode, we don't want to register or store data for push notifications
-    // to avoid duplicated notifications
-    if (!__DEV__ && !!token && !setupMutation.isPending) {
-      setupMutation.mutate()
-    }
-  }, [token, setupMutation])
-
-  useEffect(() => {
     if (__DEV__) {
+      console.log('Not running notification subscription on __DEV__')
       return
     }
 
-    return subscribeDistributorMessages(async (message) => {
-      if (message.action === 'registered' && token) {
-        console.log(
-          `[expo-unified-push] registered user ${message.data.instance} with url ${message.data.url}`,
-        )
-        try {
-          await registerUnifiedPush(token, message.data)
-          await setUpData(message.data)
-        } catch (error) {
-          console.error(
-            '[expo-unified-push] error in registerUnifiedPush: ',
-            error,
-          )
-        }
-      }
-
-      if (message.action === 'unregistered' && token && upData) {
-        console.log(
-          `[expo-unified-push] unregistered user ${message.data.instance} with url ${upData.url}`,
-        )
-        try {
-          await unregisterUnifiedPush(token, upData)
-          await setUpData(null)
-        } catch (error) {
-          console.error(
-            '[expo-unified-push] error in unregisterUnifiedPush: ',
-            error,
-          )
-        }
-      }
-
-      if (message.action === 'error') {
-        console.error('[expo-unified-push] error: ', message.data)
-      }
-
-      if (message.action === 'registrationFailed') {
-        console.error('[expo-unified-push] registrationFailed: ', message.data)
-      }
-
-      if (message.action === 'message' && AppState.currentState === 'active') {
-        console.log('[expo-unified-push] message: ', message)
-        await refetchBadges()
-      }
-    })
-  }, [token, setUpData, upData, refetchBadges])
+    return subscribeDistributorMessages((message) =>
+      notificationCallbackRef.current(message),
+    )
+  }, [])
 }
