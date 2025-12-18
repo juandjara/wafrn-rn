08fd124787d39c462a2ce6b3370bbd004a2177b1
Juan D. Jara <juanorigami@gmail.com>
2025-12-18 12:20:00 +0100

    conditional babel console remove plugin

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,17 @@
 module.exports = function (api) {
   api.cache(true)
+  const plugins = ['react-native-worklets/plugin']
+  if (process.env.NODE_ENV !== 'development') {
+    plugins.push([
+      'transform-remove-console',
+      {
+        exclude: ['error', 'warn'],
+      },
+    ])
+  }
+
   return {
     presets: ['babel-preset-expo'],
-    plugins: ['react-native-worklets/plugin', 'transform-remove-console'],
+    plugins,
   }
 }
