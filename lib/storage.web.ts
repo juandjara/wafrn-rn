e648bd0e90f8b3e709679296965eaf35b09e30db
Juan D. Jara <juanorigami@gmail.com>
2026-06-02 01:55:46 +0200

    guard against non-existant localStorage (for servers)

--- lib/storage.web.ts
+++ lib/storage.web.ts
@@ -1,15 +1,27 @@
 export function getItem(key: string): string | null {
+  if (typeof localStorage === 'undefined') {
+    return null
+  }
   return localStorage.getItem(key)
 }
 
 export async function getItemAsync(key: string): Promise<string | null> {
+  if (typeof localStorage === 'undefined') {
+    return null
+  }
   return localStorage.getItem(key)
 }
 
 export async function setItemAsync(key: string, value: string): Promise<void> {
+  if (typeof localStorage === 'undefined') {
+    return
+  }
   localStorage.setItem(key, value)
 }
 
 export async function deleteItemAsync(key: string): Promise<void> {
+  if (typeof localStorage === 'undefined') {
+    return
+  }
   localStorage.removeItem(key)
 }
