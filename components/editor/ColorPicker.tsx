3e5738c2651c7ff19f43d5b4b29f5348ec2e24cd
Juan D. Jara <juanorigami@gmail.com>
2026-08-16 02:27:09 +0200

    refactor ColorPicker to use BottomSheet

--- components/editor/ColorPicker.tsx
+++ components/editor/ColorPicker.tsx
@@ -1,8 +1,9 @@
 import { MaterialCommunityIcons } from '@expo/vector-icons'
 import { clsx } from 'clsx'
 import { useState } from 'react'
-import { Modal, Pressable, ScrollView, Text, View } from 'react-native'
+import { Pressable, ScrollView, Text, View } from 'react-native'
 import colors from 'tailwindcss/colors'
+import BottomSheet from '../BottomSheet'
 import { parseCSS, oklch2hex, LCH } from 'colorizr'
 
 const COLORS = [
@@ -58,87 +59,81 @@ export default function ColorPicker({
   }
 
   return (
-    <Modal
-      visible={open}
-      animationType="slide"
-      onRequestClose={onClose}
-      transparent
+    <BottomSheet
+      className="bg-indigo-950"
+      open={open}
+      setOpen={() => onClose()}
     >
-      <View className="flex-1">
-        <Pressable className="bg-black/50 grow" onPress={onClose} />
-        <View className="bg-indigo-950 p-2">
-          <Text className="text-white text-sm font-medium">
-            Color intensity
-          </Text>
-          <ScrollView
-            contentContainerClassName="gap-3"
-            className="shrink-0 grow-0 pt-2 pb-4"
-            keyboardShouldPersistTaps="always"
-            horizontal
-          >
-            {INTENSITIES.map((i) => (
-              <Pressable
-                key={i}
-                onPress={() => setIntensity(i)}
-                className={clsx(
-                  'px-2 py-1 rounded-lg border border-gray-500 active:bg-white/10',
-                  intensity === i ? 'bg-white' : '',
-                )}
+      <View className="p-2">
+        <Text className="text-white text-sm font-medium">Color intensity</Text>
+        <ScrollView
+          contentContainerClassName="gap-3"
+          className="shrink-0 grow-0 pt-2 pb-4"
+          keyboardShouldPersistTaps="always"
+          horizontal
+        >
+          {INTENSITIES.map((i) => (
+            <Pressable
+              key={i}
+              onPress={() => setIntensity(i)}
+              className={clsx(
+                'px-2 py-1 rounded-lg border border-gray-500 active:bg-white/10',
+                intensity === i ? 'bg-white' : '',
+              )}
+            >
+              <Text
+                className={intensity === i ? 'text-gray-700' : 'text-white'}
               >
-                <Text
-                  className={intensity === i ? 'text-gray-700' : 'text-white'}
-                >
-                  {i}
-                </Text>
-              </Pressable>
-            ))}
-          </ScrollView>
-          <Text className="text-white text-sm font-medium">Color hue</Text>
-          <ScrollView
-            contentContainerClassName="gap-3"
-            className="shrink-0 grow-0 pt-2 pb-4"
-            keyboardShouldPersistTaps="always"
-            horizontal
-          >
-            {EXTRA_COLORS.map(({ value, label }) => (
-              <Pressable
-                key={value}
-                style={{ backgroundColor: value }}
-                className={`p-2 rounded-full`}
-                accessibilityLabel={label}
-                onPress={() => {
-                  onSelect(value)
-                  onClose()
-                }}
-              >
-                <MaterialCommunityIcons
-                  name="format-color-text"
-                  color="white"
-                  size={24}
-                />
-              </Pressable>
-            ))}
-            {COLORS.map((color) => (
-              <Pressable
-                key={color}
-                accessibilityLabel={color}
-                onPress={() => {
-                  onSelect(getColor(color, intensity))
-                  onClose()
-                }}
-                style={{ backgroundColor: getColor(color, intensity) }}
-                className={`p-2 rounded-full`}
-              >
-                <MaterialCommunityIcons
-                  name="format-color-text"
-                  color="white"
-                  size={24}
-                />
-              </Pressable>
-            ))}
-          </ScrollView>
-        </View>
+                {i}
+              </Text>
+            </Pressable>
+          ))}
+        </ScrollView>
+        <Text className="text-white text-sm font-medium">Color hue</Text>
+        <ScrollView
+          contentContainerClassName="gap-3"
+          className="shrink-0 grow-0 pt-2 pb-4"
+          keyboardShouldPersistTaps="always"
+          horizontal
+        >
+          {EXTRA_COLORS.map(({ value, label }) => (
+            <Pressable
+              key={value}
+              style={{ backgroundColor: value }}
+              className={`p-2 rounded-full`}
+              accessibilityLabel={label}
+              onPress={() => {
+                onSelect(value)
+                onClose()
+              }}
+            >
+              <MaterialCommunityIcons
+                name="format-color-text"
+                color="white"
+                size={24}
+              />
+            </Pressable>
+          ))}
+          {COLORS.map((color) => (
+            <Pressable
+              key={color}
+              accessibilityLabel={color}
+              onPress={() => {
+                onSelect(getColor(color, intensity))
+                onClose()
+              }}
+              style={{ backgroundColor: getColor(color, intensity) }}
+              className={`p-2 rounded-full`}
+            >
+              <MaterialCommunityIcons
+                name="format-color-text"
+                color="white"
+                size={24}
+              />
+            </Pressable>
+          ))}
+        </ScrollView>
       </View>
-    </Modal>
+    </BottomSheet>
   )
 }
