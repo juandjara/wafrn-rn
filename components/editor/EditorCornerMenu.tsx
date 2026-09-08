692d6b727a17996f00429e79d6c721ce55bd7df6
Juan D. Jara <juanorigami@gmail.com>
2026-09-09 00:38:25 +0200

    add descriptions to items in the editor corner menu

--- components/editor/EditorCornerMenu.tsx
+++ components/editor/EditorCornerMenu.tsx
@@ -1,10 +1,8 @@
 import { MaterialCommunityIcons, MaterialIcons } from '@expo/vector-icons'
-import { View } from 'react-native'
+import { Pressable, Text, View } from 'react-native'
 import { useState } from 'react'
 import { useCSSString } from '@/lib/cssVariables'
 import BottomSheet from '../BottomSheet'
-import MenuItem from '../MenuItem'
-import { optionStyleBig } from '@/lib/styles'
 import { CreatePostPayload } from '@/lib/api/posts'
 import { PrivacyLevel } from '@/lib/api/privacy'
 import SchedulePostModal from './SchedulePostModal'
@@ -27,11 +25,14 @@ export default function EditorCornerMenu({
   const options = [
     {
       name: isDraft ? 'Make draft public' : 'Save as draft',
+      description: isDraft
+        ? 'Change privacy to Public and publish the woot'
+        : 'Change privacy to Draft and save the woot',
       icon: (
         <MaterialCommunityIcons
           name={isDraft ? 'earth' : 'archive-edit-outline'}
           color={gray600}
-          size={20}
+          size={24}
         />
       ),
       action: () => {
@@ -42,11 +43,13 @@ export default function EditorCornerMenu({
     },
     {
       name: 'Add to queue',
+      description:
+        'Add the woot to a slow queue and the server will publish it when it has some alone time (min. 1 hour)',
       icon: (
         <MaterialCommunityIcons
           name="layers-outline"
           color={gray600}
-          size={20}
+          size={24}
         />
       ),
       action: () => {
@@ -55,7 +58,9 @@ export default function EditorCornerMenu({
     },
     {
       name: 'Schedule post',
-      icon: <MaterialIcons name="schedule" color={gray600} size={20} />,
+      description:
+        'Schedule your woot to published at a certian date and time. This is checked in the server every 15 minutes so there might be some small delay in minutes',
+      icon: <MaterialIcons name="schedule" color={gray600} size={24} />,
       action: () => {
         setScheduleModalOpen(true)
       },
@@ -75,16 +80,20 @@ export default function EditorCornerMenu({
         <BottomSheet open setOpen={setOpen}>
           <View>
             {options.map((opt, i) => (
-              <MenuItem
+              <Pressable
                 key={i}
-                action={() => {
+                className="bg-white active:bg-gray-200 p-4 flex-row gap-4"
+                onPress={() => {
                   setOpen(false)
                   opt.action()
                 }}
-                icon={opt.icon}
-                label={opt.name}
-                style={optionStyleBig(i)}
-              />
+              >
+                {opt.icon}
+                <View className="grow shrink mr-2">
+                  <Text className="font-bold mb-1">{opt.name}</Text>
+                  <Text>{opt.description}</Text>
+                </View>
+              </Pressable>
             ))}
           </View>
         </BottomSheet>
